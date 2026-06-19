faebeb31ae6a02f9c91766a045c5286b53319a14
MohamedMagdy <ymmagdyfofo@gmail.com>
2026-06-19 18:31:18 +0300

    implemented jwt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,13 @@
 import userRepository from "../repositories/userRepository.js";
 
+import JWT from "jsonwebtoken";
+
 const authController = {
   register: async (req, res) => {
     try {
+      // create new user
       const newUser = await userRepository.create(req.body);
+
       res
         .status(201)
         .json({ message: "User registered successfully", user: newUser });
@@ -14,8 +18,28 @@ const authController = {
     }
   },
   login: async (req, res) => {
-    // Handle login logic here
-    res.status(200).json({ message: "Login successful" });
+    try {
+      const { email, password } = req.body;
+
+      // 1) get user
+      const user = await userRepository.findByEmail(email);
+
+      // 2) if user not found or password is incorrect
+      if (!user || !(await user.checkPassword(password))) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      // 3) generate token
+      const token = JWT.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.TOKEN_EXPIRES_IN || "7d",
+      });
+
+      res.status(200).json({ message: "Login successful", user, token });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error logging in", error: error.message });
+    }
   },
 };
 
